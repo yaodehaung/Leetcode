e1ed894077e13238c6cc0e72cebe6b7d8bf7ce85
yaodehaung <sef1548@gmail.com>
2020-12-19 17:51:45 +0800

    fix:wq

--- src/main/java/com/fishercoder/solutions/_401.java
+++ src/main/java/com/fishercoder/solutions/_401.java
@@ -19,4 +19,18 @@ public class _401 {
             return times;
         }
     }
+    public static class Solution2 {
+        public List<String> readBinaryWatch(int num) {
+            List<String> times = new ArrayList<>();
+            for (int h = 0; h < 12; h++) {
+                for (int m = 0; m < 60; m++) {
+                    if (Integer.bitCount(h * 64 + m) == num) {
+                        times.add(String.format("%d:%02d", h,
+                                m));//%02 means to pad this two-digit decimal number on the left with zeroes
+                    }
+                }
+            }
+            return times;
+        }
+    }
 }
